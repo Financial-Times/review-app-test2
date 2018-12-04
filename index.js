d9b8e8fd54c6f39ac7cd527471c7e483d89f7ba0
Yoav Shafir <shafir.yoav@gmail.com>
2018-12-04 14:56:19 +0000

    adding scripts

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ try {
 console.log('inside --------------------------------1')
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000;
 console.log('inside --------------------------------2')
 app.get('/', (req, res) => res.send('Hello World!'))
 console.log('inside --------------------------------3')
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
 } catch (err) {
   console.log(err)
 }
