07a56dbe9a8301d1e90a9093e164942fb7aee61c
Yoav Shafir <shafir.yoav@gmail.com>
2018-12-04 12:00:51 +0000

    test14

--- reviewAppInitializer.js
+++ reviewAppInitializer.js
@@ -5,8 +5,11 @@ const denodeify = require('denodeify');
 // ENV variables.
 const pipeline = process.env.PIPELINE || 'ys-pipeline'
 const repo = process.env.REPO || 'review-app-test2'
-const githubToken = process.env.GITHUB_TOKEN || 'ed81d139b6fe9d0f334b6c3af6ece0d88b13b254'
-const herokuToken = process.env.HEROKU_TOKEN || '1b253aa3-9ebe-4b0f-b1af-f2848fb52df5'
+const githubToken = process.env.GITHUB_TOKEN
+const herokuToken = process.env.HEROKU_TOKEN
+
+console.log('==============================> ', githubToken)
+console.log('==============================> ', herokuToken)
 
 // API headers.
 const githubHeaders =  {
